d619f3e0614292023c6d1cc6c37d659296ab7c3e
João Carlos Pandolfi Santana <joaopandolfi@gmail.com>
2018-02-16 14:11:46 -0200

    First Version

---
+++ constants/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+	reverse_path:"../../../../Emescam/pediabeticobot/",
+	caffe_path:"../../RNA/caffe/crfasrnn/python-scripts",
+	caffe_script: "crfasrnn.py -i {path_in} -o {path_out}",
+	download_path: "files/download/",
+	processed_path: "files/processed/",
+	tut_img_path: "files/tutorial.png",
+	tut_img_id: "AgADAQADvKcxG_YDuEbFeXgYtbq9ubYU9y8ABEYBMoRRmxRDvfUAAgI"
+}
---
+++ constants/messages.js
@@ -0,0 +1,6 @@
+module.exports = {
+	hello: "Fala {name} Beleza? \n Sou o bot responsável por gerir as atualizações do servidor",
+	blz: "Então é nois mano",
+	received_image: "Olá {name} eu recebi a sua imagem, Guardarei com muito carinho! \n",
+	default: "Oi {name} eu ainda não aprendi sobre isso, mas você pode me mandar uma imagem para eu processar :D"
+}
